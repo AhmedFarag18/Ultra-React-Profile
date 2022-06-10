0712596eac572f30e6955f84693bc432fa117dcb
AhmedFarag18 <ahmedfaragfdfdfd@gmail.com>
2022-06-10 22:14:08 +0200

    updates routes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
                 </Logo>
                 
                 <UlList>
-                    <ListItem><NavbarLink to="/index">Home</NavbarLink></ListItem>
+                    <ListItem><NavbarLink to="/">Home</NavbarLink></ListItem>
                     <ListItem><Anchor href="#">Work</Anchor></ListItem>
                     <ListItem><Anchor href="#">Portfolio</Anchor></ListItem>
                     <ListItem><Anchor href="#">Resume</Anchor></ListItem>
